3ec2a29e0910ec4822b1ac154c1c1a7dac45db62
Suheib Marzouka <suheib.marzouka@outlook.de>
2026-07-14 11:52:28 +0200

    ssh key der maschinen angehängt

--- main.tf
+++ main.tf
@@ -6,5 +6,6 @@ resource "aws_instance" "erste_instanz" {
   count         = var.instance_count
   ami           = "ami-0303e2e4a29f041a3"
   instance_type = var.aws_instance_type
+  key_name      = "25-09-key"
 }
 
